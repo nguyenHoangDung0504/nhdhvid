285bdd07f5424ce03c5f5eaea1e10dc42eb4fde5
nguyenHoangDung0504 <127297094+nguyenHoangDung0504@users.noreply.github.com>
2024-04-24 15:51:01 +0000

    24/4/2024 10:51PM

--- utils.js
+++ utils.js
@@ -2,14 +2,18 @@ function checkName(n1, n2) {
     if (n1 === "" || n2 === "") return null;
 
     let same = 0,
-        maxLength = Math.max(n1.length, n2.length);
+        maxLength = Math.min(n1.length, n2.length),
+        threshold = 0.6 * Math.max(n1.length, n2.length),
+        commonPart = "";
 
     for (let i = 0; i < maxLength; i++) {
         if (n1[i] === n2[i]) {
+            commonPart += n1[i];
             same++;
+        } else {
+            break; // Thoát khỏi vòng lặp nếu gặp ký tự khác nhau
         }
     }
 
-    let similarity = same / maxLength;
-    return similarity > 0.6 ? n1.substring(0, same) : null;
+    return same >= threshold ? commonPart : null;
 }
